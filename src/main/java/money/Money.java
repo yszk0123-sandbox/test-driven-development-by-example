c0afdc37f525988677ff141379c371e8d7ad43e0
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-19 18:38:55 +0900

    ch09

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,8 +2,18 @@ package money;
 
 abstract class Money {
   protected int amount;
+  protected String currency;
+
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
   abstract Money times(int multiplier);
+
+  String currency() {
+    return currency;
+  }
 
   public boolean equals(Object object) {
     Money money = (Money) object;
@@ -11,10 +21,10 @@ abstract class Money {
   }
 
   public static Money dollar(int amount) {
-    return new Dollar(amount);
+    return new Dollar(amount, "USD");
   }
 
   public static Franc franc(int amount) {
-    return new Franc(amount);
+    return new Franc(amount, "CHF");
   }
 }
